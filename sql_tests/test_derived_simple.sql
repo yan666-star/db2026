062f1cfecda2ee6a3ca9ad633c70e39d267a4850
compromise729 <2604082080@qq.com>
2026-06-02 17:09:30 +0800

    整理文档

--- sql_tests/test_derived_simple.sql
+++
@@ -1,5 +0,0 @@
--- single subquery derived table (non-union)
-CREATE TABLE dt1 (a INT, b INT);
-INSERT INTO dt1 VALUES (2, 20);
-INSERT INTO dt1 VALUES (1, 10);
-SELECT * FROM (SELECT * FROM dt1) AS t ORDER BY b DESC;
